5262192f09e4f3611959038e24415cfe054c5bdd
Bahna David Benaimin <bahna.benu@yahoo.com>
2026-02-08 20:36:02 +0200

    I added the motto and objective part

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 'use strict';
 
+// typed js
+let typing = new Typed(".typing",{
+    strings : ["AUTENTICĂ", "UNICĂ", "MAGICĂ", "SPECIALĂ", "SPUSĂ ÎN STILUL TĂU", "LEGATĂ DE PROMISIUNI"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true
+})
+
+
+// scrool butoon top
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 580) {
+    scrollTopBtn.classList.add("active");
+  } else {
+    scrollTopBtn.classList.remove("active");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
+
+
+
+
 
